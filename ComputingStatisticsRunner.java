b57a09a661b3326e12b14bcef6a37457c5fb2648
Kirsten Fournier <kfournier@hopkintonk12.ma.us>
2021-03-16 15:21:58 -0400

    class lab files

--- ComputingStatisticsRunner.java
+++ ComputingStatisticsRunner.java
@@ -27,6 +27,7 @@ public class ComputingStatisticsRunner {
 //       System.out.println(analysis.largestLoanKenya());
 //       System.out.println(analysis.avgLoanPhilippines());
 //       System.out.println(analysis.longestToFundCountry());
+//       System.out.println(analysis.mostLoansFunded());
 //       System.out.println(analysis.variance());
 //       System.out.println(analysis.standardDeviation());
 //       System.out.println(analysis.empiricalRule());
